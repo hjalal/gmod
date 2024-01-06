1994817d95841b5e367bf5cc61facccc770b0b0d
hjalal <hawrejalal@gmail.com>
2024-01-06 07:23:03 -0500

    simple changes to the markov example

--- markov_timedep_example.R
+++ markov_timedep_example.R
@@ -2,7 +2,7 @@
 rm(list = ls())
 #source("functions.R")
 library(tidyverse)
-library(gmod)
+library(gmod) #Grammar of Modelling 
 # Parameters:
 pProgNoTrt <-	0.100
 rrProgTrtA<-	0.8
@@ -22,58 +22,53 @@ rrProg <- function(decision){
   #df_rrProg$value[df_rrProg$decision == decision]
 }
 
-pDie <- function(state, cycle, cycle_in_state){
+pDie <- function(state){
   #state <- get("state", envir = parent.frame())  # Get x from the calling environment
-  switch(state, "Moderate" = pMortMod,
+  switch(state, 
+         "Moderate" = pMortMod,
          "Severe" = pMortSev, 
          "Dead" = 1)
   #df_pDie$value[df_pDie$state == state]
 }
 
-pProg <- function(state, decision, DIE){
+pProg <- function(state, decision){
   #decision <- get("decision", envir = parent.frame())  # Get x from the calling environment
   #state <- get("state", envir = parent.frame())  # Get x from the calling environment
   (state=="Moderate")*rrProg(decision)*pProgNoTrt
 }
 
 cost <- function(state){
-  switch(state, "Moderate" = 3000,
+  switch(state, 
+         "Moderate" = 3000,
          "Severe" = 6000,
          "Dead" = 0)
 }
 
 effectiveness <- function(state){
-  switch(state, "Moderate" = 0.8,
+  switch(state, 
+         "Moderate" = 0.8,
          "Severe" = 0.6,
          "Dead" = 0)
 }
 
-cProg <- 1000
-cDead <- 50
-cSevere <- 100000
-cStay <- 600
-eProg <- -0.1
+
 # Markov Model ==========
 mygmod <- gmod(model_type = "Markov", n_cycles = 3) + 
   decisions("NoTrt", "TrtA", "TrtB") + 
   states("Moderate", "Severe", "Dead") + 
-  #events("DIE", "PROGRESS") +
   discounts(payoffs = c("cost", "effectiveness"), discounts = c(0.015, 0.015)) + 
-  initial_probs(states = c("Moderate", "Severe"), probs = c(0.5,0.5)) +  
+  initial_probs(states = "Moderate", probs = 1) +  
   event_mapping(event = "DIE",  
             values = c(T, F), 
             results = c("Dead", "PROGRESS"), 
-            probs = c(pDie(state), Inf), 
-            payoffs = list(cost = c(cDead, cProg), effectiveness = c(NA, eProg))) +
+            probs = c(pDie(state), Inf)) +  
   event_mapping(event = "PROGRESS", 
             values = c(T, F), 
             results = c("Severe", curr_state()), 
-            #probs = c((state=="Moderate")*rrProg(decision)*pProgNoTrt, Inf))
-            probs = c(pProg(state, decision, prev_event("DIE")), Inf),
-            payoffs = list(cost = c(cSevere, cStay))) + 
-            #probs = c(pProg(state, decision), Inf)) + 
+            probs = c(pProg(state, decision), Inf)) + 
   payoffs(cost = cost(state), 
-          effectiveness = effectiveness(state))
+          effectiveness = effectiveness(state), 
+          LE = LE(cycle))
 tunnel_states(mygmod)
 is_cycle_dep(mygmod)
 model_struc <- gmod_build(mygmod)
